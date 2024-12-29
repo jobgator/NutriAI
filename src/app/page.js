0d6c716c4868e09f247c1876fd93e6e1cd9955fe
Benjamin Gelb <bengelb7@gmail.com>
2024-12-29 05:55:53 +0100

    pleasee

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@
 //import Card from '@/components/ui/card';
 //import Button from '@/components/ui/button';
 //import Input from '@/components/ui/input';
-import Label from '@/components/ui/label';
-
+//import Label from '@/components/ui/label';
+'use client';
 import Link from "next/link";
 import { SignedIn, SignedOut, SignUpButton, SignOutButton } from '@clerk/nextjs';
 
