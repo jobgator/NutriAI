2df9e7550004c983f35fa990f43a641d4d37aa2a
Benjamin Gelb <bengelb7@gmail.com>
2024-12-29 05:16:29 +0100

    fixking errors

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ import { SignedIn, SignedOut, SignUpButton, SignOutButton } from '@clerk/nextjs'
 export default function Home() {
     return (
         <main>
-            <MealPlanner />
+
         </main>
     );
 }
